3f46cc7ef4f7e77162bd84e2a1e7a863f04ddcfc
possatogabriel <chaymitoyt@gmail.com>
2024-06-06 14:56:29 -0300

    calculadora

---
+++ Calculadora/build/classes/calculadora/Calculadora.rs
@@ -0,0 +1,9 @@
+Calculadora
+Calculadora$5
+Calculadora$6
+Calculadora$7
+Calculadora$8
+Calculadora$1
+Calculadora$2
+Calculadora$3
+Calculadora$4
---
+++ Calculadora/src/Divisao.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno CA
+ */
+public class Divisao {
+    
+    int numero1, numero2;
+    
+    void setNumero1(int num1) {
+        this.numero1 = num1;
+    }
+    
+    void setNumero2(int num2) {
+        this.numero2 = num2;
+    }
+    
+    int dividir(){
+        return numero1 / numero2;
+    }
+    
+}
